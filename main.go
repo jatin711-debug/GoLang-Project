ddabbb6274989e1d86b7299dfbc103c6fc778d24
jatin711-debug <69040077+jatin711-debug@users.noreply.github.com>
2022-11-05 17:04:10 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+const portNumber string = ":8080"
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.page.tmpl")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {	
+	renderTemplate(w, "about.page.tmpl")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ :=  template.ParseFiles("./templates/"+ tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Error executing parsed file temp",err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s",portNumber))
+	_ = http.ListenAndServe(portNumber,nil);
+}
